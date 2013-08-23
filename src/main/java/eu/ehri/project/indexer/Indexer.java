bbe98a1f453ec70573fb577b34682c4d1fd5ba41
Mike Bryant <mikesname@gmail.com>
2013-08-23 20:11:58 +0100

    Use temp file to hold converted JSON. General cleanups

--- src/main/java/eu/ehri/project/indexer/Indexer.java
+++ src/main/java/eu/ehri/project/indexer/Indexer.java
@@ -13,7 +13,6 @@ import javax.ws.rs.core.Response;
 import java.io.*;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Mike Bryant (http://github.com/mikesname)
@@ -26,6 +25,10 @@ public class Indexer {
 
     public static final int BATCH_SIZE = 2500;
 
+    private static class Stats {
+        public int itemCount = 0;
+    }
+
     public static void commit() {
         WebResource commitResource = client.resource
                 ("http://localhost:8983/solr/portal/update?commit=true&optimize=true");
@@ -37,23 +40,28 @@ public class Indexer {
         }
     }
 
-    public static void doBatch(WebResource resource, List<Map<String,Object>> data) {
+    public static void doIndex(File data, boolean doCommit) {
+        WebResource resource = client.resource
+                ("http://localhost:8983/solr/portal/update?commit=" + doCommit);
         try {
-            ClientResponse response = resource
-                    .type(MediaType.APPLICATION_JSON)
-                    .entity(writer.writeValueAsString(data))
-                    .post(ClientResponse.class);
-            if (Response.Status.OK.getStatusCode() != response.getStatus()) {
-                throw new RuntimeException("Error with Solr upload: " + response.getEntity(String.class));
+            InputStream ios = new FileInputStream(data);
+            try {
+                ClientResponse response = resource
+                        .type(MediaType.APPLICATION_JSON)
+                        .entity(ios)
+                        .post(ClientResponse.class);
+                if (Response.Status.OK.getStatusCode() != response.getStatus()) {
+                    throw new RuntimeException("Error with Solr upload: " + response.getEntity(String.class));
+                }
+            } finally {
+                ios.close();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void printType(String type) throws IOException{
-
-        PrintWriter pw = new PrintWriter(System.out, true);
+    public static void printType(String type, OutputStream out, Stats stats) throws IOException{
 
         WebResource fetchResource = client.resource("http://localhost:7474/ehri/" + type + "/list?limit=100000");
 
@@ -67,9 +75,8 @@ public class Indexer {
         JsonParser jp = f.createJsonParser(br);
 
         jp.nextToken();
-        int items = 0;
 
-        JsonGenerator generator = f.createJsonGenerator(pw);
+        JsonGenerator generator = f.createJsonGenerator(out);
         try {
             generator.writeStartArray();
             generator.writeRaw('\n');
@@ -90,92 +97,57 @@ public class Indexer {
                     writer.writeValue(generator, JsonConverter.getData(node));
                     generator.writeRaw('\n');
                 }
+                stats.itemCount++;
             }
             generator.writeEndArray();
+            generator.writeRaw('\n');
         } finally {
             generator.flush();
             generator.close();
         }
-        pw.flush();
-        //System.out.println("DONE");
-        pw.close();
         jp.close();
         response.close();
     }
 
 
 
-    public static void indexType(String type) throws IOException{
+    public static void indexType(String type, File temp) throws IOException{
 
         System.out.println("Indexing: " + type);
-
         long startTime = System.nanoTime();
-
-        WebResource fetchResource = client.resource("http://localhost:7474/ehri/" + type + "/list?limit=100000");
-        WebResource postResource = client.resource("http://localhost:8983/solr/portal/update?commit=false&wt=json");
-
-        ClientResponse response = fetchResource.accept(MediaType.APPLICATION_JSON)
-                .type(MediaType.APPLICATION_JSON)
-                .get(ClientResponse.class);
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntityInputStream()));
-
-        JsonFactory f = new JsonFactory();
-        JsonParser jp = f.createJsonParser(br);
-        // advance stream to START_ARRAY first:
-        jp.nextToken();
-        int items = 0;
-
-        List<Map<String,Object>> buffer = Lists.newArrayList();
-
-        // and then each time, advance to opening START_OBJECT
-        while (jp.nextToken() == JsonToken.START_OBJECT) {
-            //RestItem doc = mapper.readValue(jp, RestItem.class);
-            JsonNode node = mapper.readValue(jp, JsonNode.class);
-
-            Iterator<JsonNode> elements = node.path("relationships").path("describes").getElements();
-            List<JsonNode> descriptions = Lists.newArrayList(elements);
-
-            if (descriptions.size() > 0) {
-                for (JsonNode description : descriptions) {
-                    buffer.add(JsonConverter.getDescribedData(description, node));
-                }
-            } else {
-                buffer.add(JsonConverter.getData(node));
-            }
-
-            items++;
-
-            if (buffer.size() >= BATCH_SIZE) {
-                System.out.println("Writing batch...");
-                doBatch(postResource, buffer);
-                buffer.clear();
-            }
-        }
-        jp.close();
-        response.close();
-
-        if (buffer.size() > 0) {
-            System.out.println("Writing final batch...");
-            doBatch(postResource, buffer);
-        }
-
-        System.out.println("Committing");
-        commit();
-
+        Stats stats = new Stats();
+        OutputStream out = new FileOutputStream(temp);
+        printType(type, out, stats);
+        doIndex(temp, false);
 
         long endTime = System.nanoTime();
         double duration = ((double)(endTime - startTime)) / 1000000000.0;
 
         System.out.println("Indexing completed in " + duration);
-        System.out.println("Items indexed: " + items);
-        System.out.println("Items per second: " + (items / duration));
+        System.out.println("Items indexed: " + stats.itemCount);
+        System.out.println("Items per second: " + (stats.itemCount / duration));
+    }
+
+    public static void print(String[] types) throws IOException {
+        OutputStream pw = new PrintStream(System.out);
+        for (String type :  types) {
+            printType(type, pw, new Stats());
+        }
+    }
+
+    public static void index(String[] types) throws IOException {
+        for (String type :  types) {
+            File temp = File.createTempFile(type, "json");
+            try {
+                indexType(type, temp);
+            } finally {
+                temp.delete();
+            }
+        }
+        commit();
     }
 
     public static void main(String[] args) throws IOException {
-
-        for (String type :  args) {
-            indexType(type);
-        }
+        index(args);
     }
 }
