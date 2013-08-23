a1dd14bcfcbb84310c6ff3c56cb348b4f8d9d8ee
Mike Bryant <mikesname@gmail.com>
2013-08-23 20:23:28 +0100

    More refactoring

--- src/main/java/eu/ehri/project/indexer/Indexer.java
+++ src/main/java/eu/ehri/project/indexer/Indexer.java
@@ -23,8 +23,6 @@ public class Indexer {
     private static ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
     private static Client client = Client.create();
 
-    public static final int BATCH_SIZE = 2500;
-
     private static class Stats {
         public int itemCount = 0;
     }
@@ -40,24 +38,15 @@ public class Indexer {
         }
     }
 
-    public static void doIndex(File data, boolean doCommit) {
+    public static void doIndex(InputStream ios, boolean doCommit) {
         WebResource resource = client.resource
                 ("http://localhost:8983/solr/portal/update?commit=" + doCommit);
-        try {
-            InputStream ios = new FileInputStream(data);
-            try {
-                ClientResponse response = resource
-                        .type(MediaType.APPLICATION_JSON)
-                        .entity(ios)
-                        .post(ClientResponse.class);
-                if (Response.Status.OK.getStatusCode() != response.getStatus()) {
-                    throw new RuntimeException("Error with Solr upload: " + response.getEntity(String.class));
-                }
-            } finally {
-                ios.close();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        ClientResponse response = resource
+                .type(MediaType.APPLICATION_JSON)
+                .entity(ios)
+                .post(ClientResponse.class);
+        if (Response.Status.OK.getStatusCode() != response.getStatus()) {
+            throw new RuntimeException("Error with Solr upload: " + response.getEntity(String.class));
         }
     }
 
@@ -74,51 +63,79 @@ public class Indexer {
         JsonFactory f = new JsonFactory();
         JsonParser jp = f.createJsonParser(br);
 
-        jp.nextToken();
-
-        JsonGenerator generator = f.createJsonGenerator(out);
         try {
-            generator.writeStartArray();
-            generator.writeRaw('\n');
+            jp.nextToken();
 
-            while (jp.nextToken() == JsonToken.START_OBJECT) {
-
-                JsonNode node = mapper.readValue(jp, JsonNode.class);
-
-                Iterator<JsonNode> elements = node.path("relationships").path("describes").getElements();
-                List<JsonNode> descriptions = Lists.newArrayList(elements);
-
-                if (descriptions.size() > 0) {
-                    for (JsonNode description : descriptions) {
-                        writer.writeValue(generator, JsonConverter.getDescribedData(description, node));
-                        generator.writeRaw('\n');
-                    }
-                } else {
-                    writer.writeValue(generator, JsonConverter.getData(node));
-                    generator.writeRaw('\n');
+            JsonGenerator generator = f.createJsonGenerator(out);
+            try {
+                generator.writeStartArray();
+                generator.writeRaw('\n');
+                while (jp.nextToken() == JsonToken.START_OBJECT) {
+                    JsonNode node = mapper.readValue(jp, JsonNode.class);
+                    convertItem(node, generator);
+                    stats.itemCount++;
                 }
-                stats.itemCount++;
+                generator.writeEndArray();
+                generator.writeRaw('\n');
+            } finally {
+                generator.flush();
+                generator.close();
             }
-            generator.writeEndArray();
-            generator.writeRaw('\n');
         } finally {
-            generator.flush();
-            generator.close();
+            jp.close();
         }
-        jp.close();
         response.close();
     }
 
+    /**
+     * Convert a individual item from the stream and write the results.
+     * @param node
+     * @param generator
+     * @throws IOException
+     */
+    private static void convertItem(JsonNode node, JsonGenerator generator) throws IOException {
+        Iterator<JsonNode> elements = node.path("relationships").path("describes").getElements();
+        List<JsonNode> descriptions = Lists.newArrayList(elements);
+
+        if (descriptions.size() > 0) {
+            for (JsonNode description : descriptions) {
+                writer.writeValue(generator, JsonConverter.getDescribedData(description, node));
+                generator.writeRaw('\n');
+            }
+        } else {
+            writer.writeValue(generator, JsonConverter.getData(node));
+            generator.writeRaw('\n');
+        }
+    }
 
 
+    /**
+     * Index a specific entity type.
+     * @param type
+     * @param temp
+     * @throws IOException
+     */
     public static void indexType(String type, File temp) throws IOException{
 
         System.out.println("Indexing: " + type);
         long startTime = System.nanoTime();
         Stats stats = new Stats();
+
+        // Write the converted JSON to the temp file...
         OutputStream out = new FileOutputStream(temp);
-        printType(type, out, stats);
-        doIndex(temp, false);
+        try {
+            printType(type, out, stats);
+        } finally {
+             out.close();
+        }
+
+        // Load the temp file as an input stream and index it...
+        InputStream ios = new FileInputStream(temp);
+        try {
+            doIndex(ios, false);
+        } finally {
+            ios.close();
+        }
 
         long endTime = System.nanoTime();
         double duration = ((double)(endTime - startTime)) / 1000000000.0;
@@ -130,8 +147,12 @@ public class Indexer {
 
     public static void print(String[] types) throws IOException {
         OutputStream pw = new PrintStream(System.out);
-        for (String type :  types) {
-            printType(type, pw, new Stats());
+        try {
+            for (String type :  types) {
+                printType(type, pw, new Stats());
+            }
+        } finally {
+            pw.close();
         }
     }
 
