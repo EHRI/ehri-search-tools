b96b13035afcc93a3f07458d113333515604f7bd
Mike Bryant <mikesname@gmail.com>
2013-08-24 19:09:28 +0100

    Refactoring to allow id-based indexing/printing

--- src/main/java/eu/ehri/project/indexer/Indexer.java
+++ src/main/java/eu/ehri/project/indexer/Indexer.java
@@ -1,5 +1,6 @@
 package eu.ehri.project.indexer;
 
+import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
@@ -23,7 +24,7 @@ public class Indexer {
 
     /**
      * Default service end points.
-     *
+     * <p/>
      * TODO: Store these in a properties file?
      */
     public static final String DEFAULT_SOLR_URL = "http://localhost:8983/solr/portal";
@@ -79,7 +80,18 @@ public class Indexer {
      * A class for holding interesting stats.
      */
     private static class Stats {
+        private long startTime = System.nanoTime();
+
         public int itemCount = 0;
+
+        public void printReport() {
+            long endTime = System.nanoTime();
+            double duration = ((double) (endTime - startTime)) / 1000000000.0;
+
+            System.out.println("Indexing completed in " + duration);
+            System.out.println("Items indexed: " + itemCount);
+            System.out.println("Items per second: " + (itemCount / duration));
+        }
     }
 
     /**
@@ -120,57 +132,39 @@ public class Indexer {
     /**
      * Write converted JSON data to an output stream.
      *
-     * @param type  The type of item to reindex
+     * @param in    The type of item to reindex
      * @param out   The output stream for converted JSON
      * @param stats A Stats object for storing metrics
      * @throws IOException
      */
-    private void convertType(String type, OutputStream out, Stats stats) throws IOException {
+    private void convertStream(InputStream in, OutputStream out, Stats stats) throws IOException {
 
-        WebResource resource = client.resource(
-            UriBuilder.fromPath(ehriUrl).segment(type).segment("list").build());
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
-        ClientResponse response = resource
-                .queryParam("limit", "100000") // Ugly, but there's a default limit
-                .accept(MediaType.APPLICATION_JSON)
-                .type(MediaType.APPLICATION_JSON)
-                .get(ClientResponse.class);
+        JsonFactory f = new JsonFactory();
+        JsonParser jp = f.createJsonParser(br);
 
         try {
-            if (response.getStatus() != Response.Status.OK.getStatusCode()) {
-                throw new RuntimeException("Unexpected response from EHRI REST: " + response.getStatus());
-            }
+            jp.nextToken();
 
-            BufferedReader br = new BufferedReader(
-                    new InputStreamReader(response.getEntityInputStream()));
-
-            JsonFactory f = new JsonFactory();
-            JsonParser jp = f.createJsonParser(br);
-
+            JsonGenerator generator = f.createJsonGenerator(out);
             try {
-                jp.nextToken();
-
-                JsonGenerator generator = f.createJsonGenerator(out);
-                try {
-                    generator.writeStartArray();
-                    generator.writeRaw('\n');
-                    while (jp.nextToken() == JsonToken.START_OBJECT) {
-                        JsonNode node = mapper.readValue(jp, JsonNode.class);
-                        convertItem(node, generator);
-                        stats.itemCount++;
-                    }
-                    generator.writeEndArray();
-                    generator.writeRaw('\n');
-                } finally {
-                    generator.flush();
-                    generator.close();
+                generator.writeStartArray();
+                generator.writeRaw('\n');
+                while (jp.nextToken() == JsonToken.START_OBJECT) {
+                    JsonNode node = mapper.readValue(jp, JsonNode.class);
+                    convertItem(node, generator);
+                    stats.itemCount++;
                 }
+                generator.writeEndArray();
+                generator.writeRaw('\n');
             } finally {
-                jp.close();
-                br.close();
+                generator.flush();
+                generator.close();
             }
         } finally {
-            response.close();
+            jp.close();
+            br.close();
         }
     }
 
@@ -199,23 +193,22 @@ public class Indexer {
 
 
     /**
-     * Index a specific entity type.
+     * Index a specific entity jobId.
      *
-     * @param type The type of item to index
+     * @param stream An input stream consisting of a JSON list
+     * @param jobId  The jobId of item to index
+     * @param stats  A statistics object
      * @throws IOException
      */
-    public void indexType(String type) throws IOException {
+    private void indexStream(InputStream stream, String jobId, Stats stats) throws IOException {
 
-        System.out.println("Indexing: " + type);
-        long startTime = System.nanoTime();
-        Stats stats = new Stats();
-
-        File tempFile = File.createTempFile(type, "json");
+        File tempFile = File.createTempFile(jobId, "json");
         try {
             // Write the converted JSON to the tempFile file...
             OutputStream out = new FileOutputStream(tempFile);
+
             try {
-                convertType(type, out, stats);
+                convertStream(stream, out, stats);
             } finally {
                 out.close();
             }
@@ -230,14 +223,28 @@ public class Indexer {
         } finally {
             tempFile.delete();
         }
+    }
 
-
-        long endTime = System.nanoTime();
-        double duration = ((double) (endTime - startTime)) / 1000000000.0;
-
-        System.out.println("Indexing completed in " + duration);
-        System.out.println("Items indexed: " + stats.itemCount);
-        System.out.println("Items per second: " + (stats.itemCount / duration));
+    /**
+     * Print converted data to StdOut.
+     *
+     * @param ids An array of item id strings
+     * @throws IOException
+     */
+    public void printIds(String[] ids) throws IOException {
+        OutputStream pw = new PrintStream(System.out);
+        Stats stats = new Stats();
+        try {
+            ClientResponse response = getJsonResponseForIds(ids);
+            try {
+                checkResponse(response);
+                convertStream(response.getEntityInputStream(), pw, stats);
+            } finally {
+                response.close();
+            }
+        } finally {
+            pw.close();
+        }
     }
 
     /**
@@ -246,11 +253,18 @@ public class Indexer {
      * @param types An array of type strings
      * @throws IOException
      */
-    public void print(String[] types) throws IOException {
+    public void printTypes(String[] types) throws IOException {
         OutputStream pw = new PrintStream(System.out);
+        Stats stats = new Stats();
         try {
             for (String type : types) {
-                convertType(type, pw, new Stats());
+                ClientResponse response = getJsonResponseForType(type);
+                try {
+                    checkResponse(response);
+                    convertStream(response.getEntityInputStream(), pw, stats);
+                } finally {
+                    response.close();
+                }
             }
         } finally {
             pw.close();
@@ -263,11 +277,90 @@ public class Indexer {
      * @param types An array of type strings
      * @throws IOException
      */
-    public void index(String[] types) throws IOException {
+    public void indexTypes(String... types) throws IOException {
+        System.out.println("Indexing: " + Joiner.on(", ").join(types));
+        Stats stats = new Stats();
+
         for (String type : types) {
-            indexType(type);
+            ClientResponse response = getJsonResponseForType(type);
+            try {
+                checkResponse(response);
+                indexStream(response.getEntityInputStream(), type, stats);
+            } finally {
+                response.close();
+            }
         }
         commit();
+        stats.printReport();
+    }
+
+    /**
+     * Index a set of content types.
+     *
+     * @param ids An array of item id strings
+     * @throws IOException
+     */
+    public void indexIds(String... ids) throws IOException {
+        String jobId = Joiner.on("-").join(ids);
+        System.out.println("Indexing: " + Joiner.on(", ").join(ids));
+        Stats stats = new Stats();
+
+        ClientResponse response = getJsonResponseForIds(ids);
+        try {
+            checkResponse(response);
+            indexStream(response.getEntityInputStream(), jobId, stats);
+        } finally {
+            response.close();
+        }
+
+        commit();
+        stats.printReport();
+    }
+
+    /**
+     * Check a REST API response is good.
+     *
+     * @param response  The response object to check
+     */
+    private void checkResponse(ClientResponse response) {
+        if (response.getStatus() != Response.Status.OK.getStatusCode()) {
+            throw new RuntimeException("Unexpected response from EHRI REST: " + response.getStatus());
+        }
+    }
+
+    /**
+     * Get API response for a given entity type.
+     *
+     * @param type  A REST type string
+     * @return      The response object
+     */
+    private ClientResponse getJsonResponseForType(String type) {
+        WebResource resource = client.resource(
+                UriBuilder.fromPath(ehriUrl).segment(type).segment("list").build());
+
+        return resource
+                .queryParam("limit", "100000") // Ugly, but there's a default limit
+                .accept(MediaType.APPLICATION_JSON)
+                .type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+    }
+
+    /**
+     * Get type-agnostic API response for a set of item ids.
+     *
+     * @param ids   A set of item ids
+     * @return      The response object
+     */
+    private ClientResponse getJsonResponseForIds(String[] ids) {
+        WebResource resource = client.resource(
+                UriBuilder.fromPath(ehriUrl).segment("entities").build());
+        for (String id : ids) {
+            resource = resource.queryParam("id", id);
+        }
+
+        return resource
+                .queryParam("limit", "100000") // Ugly, but there's a default limit
+                .accept(MediaType.APPLICATION_JSON)
+                .type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
     }
 
     public static void main(String[] args) throws IOException, ParseException {
@@ -275,12 +368,14 @@ public class Indexer {
         Options options = new Options();
         options.addOption("p", "print", false,
                 "Print converted JSON instead of indexing");
+        options.addOption("i", "items", false,
+                "Index items with the given ids, instead of types");
         options.addOption("s", "solr", true,
                 "Base URL for Solr service (minus the action segment)");
         options.addOption("e", "ehri", true,
                 "Base URL for EHRI REST service");
         CommandLineParser parser = new PosixParser();
-        CommandLine cmd = parser.parse( options, args);
+        CommandLine cmd = parser.parse(options, args);
 
         Indexer.Builder builder = new Indexer.Builder();
         if (cmd.hasOption("solr")) {
@@ -292,9 +387,17 @@ public class Indexer {
         Indexer indexer = builder.build();
 
         if (cmd.hasOption("print")) {
-            indexer.print(cmd.getArgs());
+            if (cmd.hasOption("items")) {
+                indexer.printIds(cmd.getArgs());
+            } else {
+                indexer.printTypes(cmd.getArgs());
+            }
         } else {
-            indexer.index(cmd.getArgs());
+            if (cmd.hasOption("items")) {
+                indexer.indexIds(cmd.getArgs());
+            } else {
+                indexer.indexTypes(cmd.getArgs());
+            }
         }
     }
 }
