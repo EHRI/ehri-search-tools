0d45d10a7fe523510c8e808dbca63600bf7e6e3c
Mike Bryant <mikesname@gmail.com>
2013-08-25 14:34:09 +0100

    Remove unused import

--- src/main/java/eu/ehri/project/indexer/Indexer.java
+++ src/main/java/eu/ehri/project/indexer/Indexer.java
@@ -1,7 +1,6 @@
 package eu.ehri.project.indexer;
 
 import com.google.common.base.Joiner;
-import com.google.common.base.Optional;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
