b5b41a4d3effc7965093ac7b52553c257a37f3b7
Mike Bryant <mikesname@gmail.com>
2013-08-26 00:35:08 +0100

    Add a stub for the most important bit to test

---
+++ src/test/java/eu/ehri/project/indexer/impl/JsonConverterTest.groovy
@@ -0,0 +1,22 @@
+package eu.ehri.project.indexer.impl
+
+/**
+ * @author Mike Bryant (http://github.com/mikesname)
+ */
+class JsonConverterTest {
+    void testConvert() {
+
+    }
+
+    void testGetDescribedData() {
+
+    }
+
+    void testGetData() {
+
+    }
+
+    void testFixDates() {
+
+    }
+}
