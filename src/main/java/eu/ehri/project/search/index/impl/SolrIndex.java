4086a02070a9122afde2fd91cb25fe8b92e51f04
Mike Bryant <mikesname@gmail.com>
2014-05-06 21:33:12 +0100

    Prevent OutOfMemory error when POSTing large index dumps to Solr (fixes #21).
    Apparently the default Jersey client does not use chunked POST encoding by
    default, so the index helper can run out of memory when, on VMs without
    lots of RAM available, it tries to send a large index file to Solr. This
    change:
     - switches the client to the non-default ApacheHttpClient
     - enables chunked encoding
    
    Tested working with heap size set to -Xmx8m. Additional formatting and
    comment cleanups.

--- src/main/java/eu/ehri/project/search/index/impl/SolrIndex.java
+++ src/main/java/eu/ehri/project/search/index/impl/SolrIndex.java
@@ -4,6 +4,10 @@ import com.google.common.collect.Lists;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.client.apache.ApacheHttpClient;
+import com.sun.jersey.client.apache.config.DefaultApacheHttpClientConfig;
 import eu.ehri.project.search.index.Index;
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonGenerator;
@@ -18,13 +22,16 @@ import java.io.StringWriter;
 import java.util.List;
 
 /**
+ * Index implementation for an Apache Solr instance. NB: We use
+ * the (non-default) Apache client instance here to avoid problems
+ * with the default implementation's handling of chunked POST data
+ * (which is necessary because we're streaming large files to Solr.)
+ *
  * @author Mike Bryant (http://github.com/mikesname)
- *         <p/>
- *         Index implementation for an Apache Solr instance.
  */
 public class SolrIndex implements Index {
 
-    private static final Client client = Client.create();
+    private static final Client client = ApacheHttpClient.create(getClientConfig());
     private static final JsonFactory jsonFactory = new JsonFactory();
 
     /**
@@ -63,6 +70,20 @@ public class SolrIndex implements Index {
     }
 
     /**
+     * Get the default client configuration. In order to prevent
+     * OutOfMemory Heap-space errors when POSTing large files to Solr
+     * we have to enable chunked encoding, which is done by setting the
+     * PROPERTY_CHUNKED_ENCODING_SIZE to a non-null value, with 0 indicating
+     * the default value.
+     */
+    private static ClientConfig getClientConfig() {
+        ClientConfig config = new DefaultClientConfig();
+        config.getProperties().put(
+                DefaultApacheHttpClientConfig.PROPERTY_CHUNKED_ENCODING_SIZE, 0);
+        return config;
+    }
+
+    /**
      * Delete everything in the index.
      *
      * @param commit Whether or not to commit the action.
@@ -88,7 +109,7 @@ public class SolrIndex implements Index {
     /**
      * Delete all items with a given field value.
      *
-     * @param field    The field name
+     * @param field  The field name
      * @param value  The field value
      * @param commit Whether or not to commit the action.
      * @throws IndexException
@@ -168,9 +189,10 @@ public class SolrIndex implements Index {
     /**
      * Generate a Solr query matching all items with the given field value,
      * i.e. "heldBy:us-005248"
-     * @param field
-     * @param value
-     * @return
+     *
+     * @param field The field key
+     * @param value The field value
+     * @return The key/value formatted as a Solr query
      */
     private String keyValueQuery(String field, String value) {
         return String.format("%s:\"%s\"", field, value);
