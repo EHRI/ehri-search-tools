8b36047ab94b6c5d12144c006709c177894d5b8b
Mike Bryant <mikesname@gmail.com>
2015-08-29 14:27:56 +0100

    Testing command-line behaviour

--- index-data-converter/src/test/java/eu/ehri/project/indexing/IndexHelperTest.java
+++ index-data-converter/src/test/java/eu/ehri/project/indexing/IndexHelperTest.java
@@ -1,8 +1,18 @@
 package eu.ehri.project.indexing;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.net.URI;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -10,6 +20,31 @@ import static org.junit.Assert.*;
  * @author Mike Bryant (http://github.com/mikesname)
  */
 public class IndexHelperTest {
+
+    // Reassign System.out/err/in to test the cmd-line output.
+    // Store the original one here.
+    private PrintStream _out;
+    private PrintStream _err;
+    private InputStream _in;
+    private ByteArrayOutputStream out;
+
+    @Before
+    public void setUp() {
+        _out = System.out;
+        _err = System.err;
+        _in = System.in;
+        out = new ByteArrayOutputStream();
+        System.setIn(getClass().getClassLoader()
+                .getResourceAsStream("inputdoc1.json"));
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(_out);
+        System.setErr(_err);
+        System.setIn(_in);
+    }
 
     @Test
     public void testUrlsFromSpecs() throws Exception {
@@ -28,5 +63,42 @@ public class IndexHelperTest {
         // An item tree, where the type is "foo" and the ID is "bar"
         assertEquals(new URI(base + "/foo/bar/list?limit=-1&all=true"),
                 IndexHelper.urlsFromSpecs(base, "foo|bar").get(0));
+    }
+
+    @Test
+    public void testInputStream() throws Exception {
+        IndexHelper.main(new String[]{"--file", "-", "--pretty"});
+
+        JsonNode node = (new ObjectMapper()).readTree(out.toByteArray());
+        assertEquals("Herta Berg: family recipe note books",
+                node.path(0).path("name").asText());
+    }
+
+    @Test
+    public void testInputStreamWithNoConversion() throws Exception {
+        IndexHelper.main(new String[]{"--file", "-", "--pretty", "--noconvert"});
+
+        JsonNode node = (new ObjectMapper()).readTree(out.toByteArray());
+        assertEquals("Herta Berg: family recipe note books", node
+                .path(0).path("relationships").path("describes")
+                .path(0).path("data").path("name").asText());
+    }
+
+    @Test
+    public void testInputStreamWithStatsAndVerbose() throws Exception {
+        ByteArrayOutputStream err = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(err));
+
+        IndexHelper.main(new String[]{"--file", "-", "--stats", "--verbose"});
+
+        JsonNode node = (new ObjectMapper()).readTree(out.toByteArray());
+        List<String> info = Lists.newArrayList(Splitter.on("\n")
+                .omitEmptyStrings().split(err.toString()));
+
+        // One item converted, plus three lines stats
+        assertEquals(4, info.size());
+        assertEquals("documentaryUnit -> eb747649-4f7b-4874-98cf-f236d2b5fa1d",
+                info.get(0));
+        assertEquals(info.get(2), "Items indexed: " + node.size());
     }
 }
