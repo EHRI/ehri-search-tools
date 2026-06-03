d8b0bca25787e0201ff529f26f5992d5dcf326d4
Mike Bryant <mikesname@gmail.com>
2026-06-03 15:27:33 +0100

    Add a PID field, along w/ some Docker config tweaks
    Bump version

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-tag=${1:-ehri/ehri-search-tools}
+TAG=${1:-ehri/ehri-search-tools}
+
+# Get project version from pom.xml
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 mvn package
-sudo docker build -t $tag .
+sudo docker build --tag $TAG:latest --tag $TAG:${VERSION} --build-arg PROJECT_VERSION=$VERSION  .
